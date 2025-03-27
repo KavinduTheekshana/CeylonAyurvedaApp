8ef52ba93bf1798a2ada3a6111f42f5146c69c55
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-03-27 21:21:04 +0000

    Remove vite

--- resources/views/html/login.blade.php
+++ resources/views/html/login.blade.php
@@ -16,7 +16,7 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="{{ asset('frontend/css/styles.css') }}">
-    @vite(['resources/js/app.js'])
+    {{-- @vite(['resources/js/app.js']) --}}
 
 
 </head>
