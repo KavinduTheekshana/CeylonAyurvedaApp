7ae96ef6e891950c3c77cbbda87e450cec05b3ec
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-05-25 20:14:30 +0100

    register route

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
-Route::post('/patient/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
 Route::post('/verify-email', [UserController::class, 'verifyEmail']);
 Route::post('/resend-verification', [UserController::class, 'resendVerificationCode']);
 Route::post('/login', [UserController::class, 'login']);
