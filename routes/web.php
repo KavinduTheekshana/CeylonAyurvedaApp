c8293310026d9abe7d870e1bd92a664f1ad5a0ec
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-05-25 18:02:53 +0100

    API

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('html.login');
-});
+// Route::get('/', function () {
+//     return view('html.login');
+// });
 
 Route::middleware([
     'auth:sanctum',
